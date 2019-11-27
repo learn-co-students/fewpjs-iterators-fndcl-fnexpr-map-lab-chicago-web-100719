290eeb459fe634e5ffa6d497ec72e41da003ddaf
NaveedG <nganjani@protonmail.com>
2019-11-27 08:51:27 -0600

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+function capitalize(sentence) {
+  return sentence.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+}
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => capitalize(tutorial))
 }
